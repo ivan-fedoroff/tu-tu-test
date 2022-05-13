81b347a7f7848986d303fb0d187051f53a6ce77f
Fedorov Ivan <sirarthurjohnevans@live.com>
2022-05-13 13:40:43 +0300

    add loading spinner and network errors handler

--- table/src/index.js
+++ table/src/index.js
@@ -19,10 +19,37 @@ const renderTable = (data) => {
   return htmlRows.join("\n");
 };
 
+const renderLoaderSpinner = (elements) => {
+  elements.tBody.innerHTML = `<div class="d-flex align-items-center">
+  <strong>Loading...</strong>
+  <div class="spinner-border text-light ms-auto" role="status" aria-hidden="true"></div>
+</div>`;
+  elements.btnShort.disabled = true;
+  elements.btnLong.disabled = true;
+  const btnDisabledHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  Loading...`;
+  elements.btnShort.innerHTML = btnDisabledHTML;
+  elements.btnLong.innerHTML = btnDisabledHTML;
+}
+
+const renderWorkedBtn = (elements) => {
+  elements.btnShort.innerHTML = 'Short table';
+  elements.btnShort.disabled = false;
+  elements.btnLong.innerHTML = 'Long table';
+  elements.btnLong.disabled = false;
+}
+
+const errorMessages = {
+  network: {
+    error: 'Network Problems. Try again.',
+  },
+};
+
 const app = async () => {
   const state = {
     data: [],
     uiState: {
+      processState: null,
       tableData: []
     }
   };
@@ -36,15 +63,43 @@ const app = async () => {
   const watchedState = onChange(state, async (path, value) => {
     if (path === "uiState.tableData") {
       elements.tBody.innerHTML = await renderTable(value);
+
+    }
+
+    if (path === 'uiState.processState') {
+      switch (value) {
+        case 'loading':
+          renderLoaderSpinner(elements);
+          break;
+
+        case 'filling':
+          renderWorkedBtn(elements);
+          break;
+
+        case 'error':
+          renderWorkedBtn(elements);
+          elements.tBody.innerHTML = `<p class="text-warning h4">${errorMessages.network.error}</p>`
+        break;
+
+        default:
+          throw new Error(`Unknown process state: ${path}`);
+      }
     }
   });
 
   elements.btnShort.addEventListener("click", async () => {
-    const response = await axios.get(
-      "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&adress=%7BaddressObject%7D&description=%7Blorem%7C32%7D"
-    );
-    watchedState.data = await response.data;
-    watchedState.uiState.tableData = await watchedState.data;
+    watchedState.uiState.processState = 'loading';
+    try {
+      const response = await axios.get(
+        "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&adress=%7BaddressObject%7D&description=%7Blorem%7C32%7D"
+      );
+      watchedState.data = await response.data;
+      watchedState.uiState.tableData = await watchedState.data;
+      watchedState.uiState.processState = 'filling'
+    } catch (error) {
+      watchedState.uiState.processState = 'error';
+    }
+    
   });
 };
 
